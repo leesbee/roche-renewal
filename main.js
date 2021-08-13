2eeb98f5327c7e6be74b79e07ff5938c5070cf79
leesbee <84954696+leesbee@users.noreply.github.com>
2021-08-13 13:51:21 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+$(function(){
+    var mySwiper = new Swiper('.swiper-container', {
+        direction: 'horizontal', // 방향
+        loop: true, //무한 반복
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          },
+        autoplay : {
+            delay : 3000
+        }
+    });
+});
+
+$(function(){
+    var $Main_active = $('.gnb > li');
+
+
+    $Main_active.hover(function(e){
+    e.preventDefault();
+            
+    $Main_active.removeClass('active');
+    $(this).addClass('active');      
+    })
+});    
+
+$(function(){
+    var $header = $('header'); //헤더를 변수에 넣기
+    var $page = $('#CompanyInfo'); //색상이 변할 부분
+    var $window = $(window);
+    var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
+    
+    $window.resize(function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
+      pageOffsetTop = $page.offset().top;
+    });
+    
+    $window.on('scroll', function(){ //스크롤시
+      var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
+      $header.toggleClass('down', scrolled); //클래스 토글
+      var scrolled = $window.scrollTop() >= page >= 100; //맨위스크롤하면
+      $header.toggleClass(false); //클래스 토글
+    });
+  });
+  
+
+  $(function(){
+    var $header = $('header'); //헤더를 변수에 넣기
+    var $page = $('#Main_nav'); //색상이 변할 부분
+    var $window = $(window);
+    var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
+    
+    $window.resize(function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
+      pageOffsetTop = $page.offset().top;
+    });
+    
+    $window.on('scroll', function(){ //스크롤시
+      var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
+      $header.toggleClass('down', scrolled); //클래스 토글
+      var scrolled = $window.scrollTop() >= page >= 100; //맨위스크롤하면
+      $header.toggleClass(false); //클래스 토글
+    });
+  });
+
+  
+
